0d8129aced01ae6c5c4e3f64be5b9cc26d61bec8
lhasadreams <adrian@chef.io>
2019-05-16 14:53:23 +0100

    Initial Commit

---
+++ cookbooks/remediate/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: base-cookbook
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+# Create the Security file
+case node['os']
+when 'linux'
+  file '/linux-check' do
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create
+  end
