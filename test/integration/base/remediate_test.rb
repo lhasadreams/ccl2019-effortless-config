48297a2cbbd2a029c5564c7d43088ab5a33011a2
lhasadreams <adrian@chef.io>
2019-05-17 10:18:55 +0100

    add test

---
+++ test/integration/base/remediate_test.rb
@@ -0,0 +1,8 @@
+# # encoding: utf-8
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+describe file('/linux-check') do
+  it { should exist }
+end
