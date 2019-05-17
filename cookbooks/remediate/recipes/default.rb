ec568f3d29b3efd0dab01d7c27350085012baad8
lhasadreams <adrian@chef.io>
2019-05-17 08:12:50 +0100

    Clear recipe

--- cookbooks/remediate/recipes/default.rb
+++ cookbooks/remediate/recipes/default.rb
@@ -3,14 +3,3 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
-
-# Create the Security file
-case node['os']
-when 'linux'
-  file '/linux-check' do
-    owner 'root'
-    group 'root'
-    mode '0644'
-    action :create
-  end
-end
